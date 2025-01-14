139d7880c164da6b76b96d04ad34a32ec705f6c0
Brian ODonnell <bodonne3@depaul.edu>
2025-01-13 19:09:58 -0600

    updated my pom file from 1.7 to 21

---
+++ week2/src/main/java/csc402/week2/App.java
@@ -0,0 +1,14 @@
+package csc402.week2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
